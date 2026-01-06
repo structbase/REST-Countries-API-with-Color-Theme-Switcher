435c52e9246dbd5fe4242a3074be8389b4ba4b92
Abenezer <abitysank@gmail.com>
2026-01-06 16:07:25 -0500

    Update to CountryCard
    Structure clean up on CountryCard and styling to the component

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,20 +1,25 @@
 import type { CountryCardProps } from "../../types/country-card-props";
+import "./CountryCard.css";
 
 export default function CountryCard({ country, onClick }: CountryCardProps) {
     return (
-        <div onClick={onClick}>
+        <div className="country-card" onClick={onClick}>
             <img src={country.flag} alt={`Flag of ${country.name}`} />
-            <h2>{country.name}</h2>
-            <p>
-                <strong>Population:</strong>{" "}
-                {country.population.toLocaleString()}
-            </p>
-            <p>
-                <strong>Region:</strong> {country.region}
-            </p>
-            <p>
-                <strong>Capital:</strong> {country.capital}
-            </p>
+
+            <div className="country-card-content">
+                <h2>{country.name}</h2>
+
+                <p>
+                    <strong>Population:</strong>{" "}
+                    {country.population.toLocaleString()}
+                </p>
+                <p>
+                    <strong>Region:</strong> {country.region}
+                </p>
+                <p>
+                    <strong>Capital:</strong> {country.capital}
+                </p>
+            </div>
         </div>
     );
 }
