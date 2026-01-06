8b054f93bd9c06154e40fb537d6a2f6e46e65814
Abenezer <abitysank@gmail.com>
2026-01-06 09:16:25 -0500

    Update CountryCard.tsx
    Wrap country card with link to forward individual country.

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,20 +1,31 @@
-import type { CountryCardProps } from "../../types/CountryCardProps";
+import type { CountryCardProps } from "../../types/country-card-props";
+import { Link } from "react-router-dom";
 
-export default function CountryCard({ country, onClick }: CountryCardProps) {
+export default function CountryCard({
+    country,
+    onClick,
+    link,
+}: CountryCardProps) {
+    const countryDetails = link || `/country/${country.name}`;
+
     return (
-        <div onClick={onClick}>
-            <img src={country.flag} alt={`Flag of ${country.name}`} />
-            <h2>{country.name}</h2>
-            <p>
-                <strong>Population:</strong>{" "}
-                {country.population.toLocaleString()}
-            </p>
-            <p>
-                <strong>Region:</strong> {country.region}
-            </p>
-            <p>
-                <strong>Capital:</strong> {country.capital}
-            </p>
+        <div>
+            <Link to={countryDetails}>
+                <div onClick={onClick}>
+                    <img src={country.flag} alt={`Flag of ${country.name}`} />
+                    <h2>{country.name}</h2>
+                    <p>
+                        <strong>Population:</strong>{" "}
+                        {country.population.toLocaleString()}
+                    </p>
+                    <p>
+                        <strong>Region:</strong> {country.region}
+                    </p>
+                    <p>
+                        <strong>Capital:</strong> {country.capital}
+                    </p>
+                </div>
+            </Link>
         </div>
     );
 }
