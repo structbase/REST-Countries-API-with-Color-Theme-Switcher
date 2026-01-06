e707d60b1a72b69d60179052fefa3d3e992066f3
Abenezer <abitysank@gmail.com>
2026-01-06 14:38:52 -0500

    Update App.tsx
    Wrap the App with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import CountryPage from "./pages/CountryPage/CountryPage";
 import Navbar from "./components/Navbar/Navbar";
+import { ThemeProvider } from "./context/theme/ThemeProvider";
 
 function App() {
     return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/country/:code" element={<CountryPage />} />
-            </Routes>
-        </>
+        <ThemeProvider>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/country/:code" element={<CountryPage />} />
+                </Routes>
+            </div>
+        </ThemeProvider>
     );
 }
 
