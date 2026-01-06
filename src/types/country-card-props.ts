d2accb0284b092dc8e387ec9adee636474a1c188
Abenezer <abitysank@gmail.com>
2026-01-06 09:08:37 -0500

    Update to types
    update to naming scheme for type files and added link for countrycardprops

--- src/types/country-card-props.ts
+++ src/types/country-card-props.ts
@@ -1,6 +1,7 @@
-import type { CountryDetail } from "./country";
+import type { CountryDetail } from "./country-detail";
 
 export interface CountryCardProps {
     country: CountryDetail;
     onClick?: () => void;
+    link?: string;
 }
